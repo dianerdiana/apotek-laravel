6e5e33faf8434dd3eda73f630621a26a45d6ba03
dianerdiana <dianerdiana.de@gmail.com>
2024-08-01 19:43:23 +0700

    Add: assets, css, & FrontController

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,31 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductTransactionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
+Route::get('/details/{product:slug}', [FrontController::class, 'details'])->name('front.product.details');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('product_transactions', ProductTransactionController::class)->middleware('role:owner|buyer');
+  Route::resource('product_transactions', ProductTransactionController::class)->middleware('role:owner|buyer');
 
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::resource('products', ProductController::class)->middleware('role:owner');
-        Route::resource('categories', CategoryController::class)->middleware('role:owner');
-    });
+  Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('products', ProductController::class)->middleware('role:owner');
+    Route::resource('categories', CategoryController::class)->middleware('role:owner');
+  });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
