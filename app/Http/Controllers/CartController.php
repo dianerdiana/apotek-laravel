80c1afb5ecf2b0e974a04cf7b5031f614b2a9248
dianerdiana <dianerdiana.de@gmail.com>
2024-08-01 21:02:15 +0700

    Handle function add to cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,63 +3,93 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class CartController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    //
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store($productId)
+  {
+    $user = Auth::user();
+    $existingCartItem = Cart::where('user_id', $user->id)->where('product_id', $productId)->first();
+
+    if ($existingCartItem) {
+      return redirect()->route('carts.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    DB::beginTransaction();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    try {
+      $cart = Cart::updateOrCreate([
+        "user_id" => $user->id,
+        'product_id' => $productId
+      ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Cart $cart)
-    {
-        //
-    }
+      $cart->save();
+      DB::commit();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Cart $cart)
-    {
-        //
-    }
+      return redirect()->route('carts.index');
+    } catch (Exception $e) {
+      DB::rollBack();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Cart $cart)
-    {
-        //
+      $error = ValidationException::withMessages([
+        'system_error' => ('System Error! ' . $e->getMessage())
+      ]);
+      throw $error;
     }
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Cart $cart)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Cart $cart)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Cart $cart)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Cart $cart)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Cart $cart)
+  {
+    //
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\FrontendController;
@@ -21,6 +22,7 @@ Route::middleware('auth')->group(function () {
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+  Route::resource('carts', CartController::class)->middleware('role:buyer');
   Route::resource('product_transactions', ProductTransactionController::class)->middleware('role:owner|buyer');
 
   Route::prefix('admin')->name('admin.')->group(function () {
